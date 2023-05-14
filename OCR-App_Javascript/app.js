2065a84656cc303c54208504a3f17022859727c6
Devanshi <devanshi3533.be20@chitkara.edu.in>
2023-05-15 00:00:31 +0530

    initial commit - image file storage done

---
+++ OCR-App_Javascript/app.js
@@ -0,0 +1,90 @@
+// const express = require('express');
+// const app= express();
+// const fs = require('fs');
+// const multer= require('multer');
+// const {TesseractWorker}= require("tesseract.js");
+// const worker = new TesseractWorker();
+
+
+// //storage for storing images 
+// const storage = multer.diskStorage({
+//     destination: (req,file,cb)=>{
+//         cb(null, "./uploads");
+//     },
+//     filename:(req,file,cb)=>{
+//         cb(null, file.originalname);
+//     }
+// });
+// const upload=multer({storage:storage}).single("avatar");
+// app.set("view engine","ejs");
+
+
+// //routes
+// app.get('/', (req,res)=>{
+//     res.render('index');
+// })
+// app.post('/upload',(req,res)=>{
+//     upload(req,res,err=>{
+//         // console.log(req.file);
+//         fs.readFile(`./uploads/${req.file.originalname}`, (err, data)=>{
+//             if(err) return console.log(" Error in reading file");
+
+//             (async () => {
+//                 await worker.load();
+//                 await worker.loadLanguage('eng');
+//                 await worker.initialize('eng');
+//                 const { data: { text } } = await worker.recognize(data);
+//                 console.log(text);
+//                 await worker.terminate();
+//                 res.send(text);
+//                 })();
+//         })
+//     })
+// })
+
+// app.listen(5002,()=>  console.log('server running on 5002'));
+
+const express = require('express');
+const app= express();
+const fs = require('fs');
+const multer= require('multer');
+const { createWorker } = require('tesseract.js');
+const worker = createWorker();
+
+//storage for storing images 
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null, "./uploads");
+    },
+    filename:(req,file,cb)=>{
+        cb(null, file.originalname);
+    }
+});
+const upload=multer({storage:storage}).single("avatar");
+app.set("view engine","ejs");
+
+
+//routes
+app.get('/', (req,res)=>{
+    res.render('index');
+})
+app.post('/upload',(req,res)=>{
+    upload(req,res,err=>{
+        console.log(req.file);
+        // fs.readFile(`./uploads/${req.file.originalname}`, (err, data)=>{
+        //     if(err) return console.log(" Error in reading file");
+
+        //     (async () => {
+        //         await worker.load();
+        //         await worker.loadLanguage('eng');
+        //         await worker.initialize('eng');
+        //         const { data: { text } } = await worker.recognize(data);
+        //         console.log(text);
+        //         await worker.terminate();
+        //         res.send(text);
+        //     })();
+        // })
+    })
+})
+
+app.listen(5002,()=>  console.log('server running on 5002'));
