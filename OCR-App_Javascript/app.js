6495538b34cf7588e0049a894003c4d9adf97299
Devanshi <devanshi3533.be20@chitkara.edu.in>
2023-05-15 18:20:27 +0530

    bug fixed- can read the picture now

--- OCR-App_Javascript/app.js
+++ OCR-App_Javascript/app.js
@@ -1,55 +1,12 @@
-// const express = require('express');
-// const app= express();
-// const fs = require('fs');
-// const multer= require('multer');
-// const {TesseractWorker}= require("tesseract.js");
-// const worker = new TesseractWorker();
-
-
-// //storage for storing images 
-// const storage = multer.diskStorage({
-//     destination: (req,file,cb)=>{
-//         cb(null, "./uploads");
-//     },
-//     filename:(req,file,cb)=>{
-//         cb(null, file.originalname);
-//     }
-// });
-// const upload=multer({storage:storage}).single("avatar");
-// app.set("view engine","ejs");
-
-
-// //routes
-// app.get('/', (req,res)=>{
-//     res.render('index');
-// })
-// app.post('/upload',(req,res)=>{
-//     upload(req,res,err=>{
-//         // console.log(req.file);
-//         fs.readFile(`./uploads/${req.file.originalname}`, (err, data)=>{
-//             if(err) return console.log(" Error in reading file");
-
-//             (async () => {
-//                 await worker.load();
-//                 await worker.loadLanguage('eng');
-//                 await worker.initialize('eng');
-//                 const { data: { text } } = await worker.recognize(data);
-//                 console.log(text);
-//                 await worker.terminate();
-//                 res.send(text);
-//                 })();
-//         })
-//     })
-// })
-
-// app.listen(5002,()=>  console.log('server running on 5002'));
-
 const express = require('express');
 const app= express();
 const fs = require('fs');
 const multer= require('multer');
-const { createWorker } = require('tesseract.js');
-const worker = createWorker();
+const { createWorker} = require('tesseract.js');
+ 
+const worker = createWorker({
+  logger: m => console.log(m)
+});
 
 //storage for storing images 
 const storage = multer.diskStorage({
@@ -70,21 +27,75 @@ app.get('/', (req,res)=>{
 })
 app.post('/upload',(req,res)=>{
     upload(req,res,err=>{
-        console.log(req.file);
-        // fs.readFile(`./uploads/${req.file.originalname}`, (err, data)=>{
-        //     if(err) return console.log(" Error in reading file");
+        // console.log(req.file);
+        fs.readFile(`./uploads/${req.file.originalname}`, (err, data)=>{
+            if(err) return console.log(" Error in reading file",err);
 
-        //     (async () => {
-        //         await worker.load();
-        //         await worker.loadLanguage('eng');
-        //         await worker.initialize('eng');
-        //         const { data: { text } } = await worker.recognize(data);
-        //         console.log(text);
-        //         await worker.terminate();
-        //         res.send(text);
-        //     })();
-        // })
+            (async () => {
+                await worker.load();
+                await worker.loadLanguage('eng');
+                await worker.initialize('eng');
+                const { data: { text } } = await worker.recognize(data);
+                console.log(text);
+                await worker.terminate();
+                res.send(text);
+                })();
+            // worker
+            // .recognize(data, "eng",{tessjs_create_pdf:"1"})
+            // .progress(progress=>{
+            //     console.log(progress);
+            // }).then(result=>{
+            //     res.send(result.text);
+            // }).finally(()=>{
+            //     worker.terminate();
+            // })
+        })
     })
 })
 
 app.listen(5002,()=>  console.log('server running on 5002'));
+
+// const express = require('express');
+// const app= express();
+// const fs = require('fs');
+// const multer= require('multer');
+// const { createWorker } = require('tesseract.js');
+// const worker = createWorker();
+
+// //storage for storing images 
+// const storage = multer.diskStorage({
+//     destination: (req,file,cb)=>{
+//         cb(null, "./uploads");
+//     },
+//     filename:(req,file,cb)=>{
+//         cb(null, file.originalname);
+//     }
+// });
+// const upload=multer({storage:storage}).single("avatar");
+// app.set("view engine","ejs");
+
+
+// //routes
+// app.get('/', (req,res)=>{
+//     res.render('index');
+// })
+// app.post('/upload',(req,res)=>{
+//     upload(req,res,err=>{
+//         console.log(req.file);
+//         // fs.readFile(`./uploads/${req.file.originalname}`, (err, data)=>{
+//         //     if(err) return console.log(" Error in reading file");
+
+//         //     (async () => {
+//         //         await worker.load();
+//         //         await worker.loadLanguage('eng');
+//         //         await worker.initialize('eng');
+//         //         const { data: { text } } = await worker.recognize(data);
+//         //         console.log(text);
+//         //         await worker.terminate();
+//         //         res.send(text);
+//         //     })();
+//         // })
+//     })
+// })
+
+// app.listen(5002,()=>  console.log('server running on 5002'));
