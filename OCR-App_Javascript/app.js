fa4f4890252c93911b51a299fe965b303da18a71
Devanshi <devanshi3533.be20@chitkara.edu.in>
2023-05-16 23:51:03 +0530

    Added lang option & separate convert, download btn

--- OCR-App_Javascript/app.js
+++ OCR-App_Javascript/app.js
@@ -1,80 +1,3 @@
-// const express = require('express');
-// const app= express();
-// const fs = require('fs');
-// const multer= require('multer');
-// const { createWorker} = require('tesseract.js');
- 
-// const worker = createWorker({
-//   logger: m => console.log(m)
-// });
-
-// //storage for storing images 
-// const storage = multer.diskStorage({
-//     destination: (req,file,cb)=>{
-//         cb(null, "./uploads");
-//     },
-//     filename:(req,file,cb)=>{
-//         cb(null, file.originalname);
-//     }
-// });
-// const upload=multer({storage:storage}).single("avatar");
-// app.set("view engine","ejs");
-
-
-// //routes
-// app.get('/', (req,res)=>{
-//     res.render('index');
-// })
-
-// const PDFDocument = require('pdfkit');
-
-// app.post('/upload', (req, res) => {
-//   upload(req, res, async (err) => {
-//     if (err) {
-//       // Handle upload error
-//       return res.status(500).send('Upload failed');
-//     }
-
-//     try {
-//       const imageData = await fs.promises.readFile(`./uploads/${req.file.originalname}`);
-
-//       const worker = createWorker();
-//       await worker.load();
-//       await worker.loadLanguage('eng');
-//       await worker.initialize('eng');
-
-//       const { data } = await worker.recognize(imageData);
-//       console.log('Recognized text:', data.text);
-
-//       const doc = new PDFDocument();
-//       doc.pipe(fs.createWriteStream('output.pdf'));
-//       doc.text(data.text);
-//       doc.end();
-
-//       await worker.terminate();
-
-//       console.log('PDF file saved successfully.');
-
-//     //   Send the recognized text and PDF file path as response
-//       res.send({
-//         text: data.text,
-//         pdfPath: 'output.pdf'
-//       });
-
-//     res.redirect('/download');
-//     } catch (error) {
-//       console.error('Error processing image:', error);
-//       res.status(500).send('Processing failed');
-//     }
-//   });
-// });
-
-// app.get('/download',(req,res)=>{
-//     const file = `${__dirname}/output.pdf`;
-//     res.download(file);
-// })
-
-// app.listen(5002,()=>  console.log('server running on 5002'));
 
 const express = require('express');
 const app = express();
@@ -82,9 +5,12 @@ const fs = require('fs');
 const multer = require('multer');
 const { createWorker } = require('tesseract.js');
 const PDFDocument = require('pdfkit');
+app.use(express.static('public'));
 displayText={
-    message:""
+    message:"",
+    languageCode:'',
 }
+
 const worker = createWorker({
   logger: m => console.log(m)
 });
@@ -116,11 +42,13 @@ app.post('/upload', (req, res) => {
 
     try {
       const imageData = await fs.promises.readFile(`./uploads/${req.file.originalname}`);
+      const languageCode = req.body.language; // Get the selected language code from the request body
+
 
       const worker = createWorker();
       await worker.load();
-      await worker.loadLanguage('eng');
-      await worker.initialize('eng');
+      await worker.loadLanguage(languageCode); // Load the selected language
+      await worker.initialize(languageCode); // Initialize the selected language
 
       const { data } = await worker.recognize(imageData);
       console.log('Recognized text:', data.text);
@@ -135,16 +63,10 @@ app.post('/upload', (req, res) => {
 
       console.log('PDF file saved successfully.');
 
-      // Send the recognized text and PDF file path as response
-    //   res.send({
-    //     text: data.text,
-    //     pdfPath: 'output.pdf'
-    //   });
 
-        displayText= {
-           message: data.text
-        }
-      res.redirect('/download');
+        displayText.message= data.text;
+
+      res.redirect('/');
     } catch (error) {
       console.error('Error processing image:', error);
       res.status(500).send('Processing failed');
@@ -154,8 +76,15 @@ app.post('/upload', (req, res) => {
 
 app.get('/download', (req, res) => {
   const file = `${__dirname}/output.pdf`;
-  res.download(file);
-  res.redirect('/');
-});
 
+  if(file){
+   res.download(file, () => {
+    console.log('PDF file downloaded successfully.');
+  }); 
+  }else{
+    res.redirect('/');
+  }
+  
+});
 app.listen(5002, () => console.log('Server running on port 5002'));
+
